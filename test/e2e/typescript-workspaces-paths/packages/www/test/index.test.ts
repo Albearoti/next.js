7acfd8b4d63e4793a34bc56b28f7c672ccf9518b
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-04-07 18:53:48 +0200

    Test typescript-workspaces-paths with and without baseUrl

--- test/e2e/typescript-workspaces-paths/packages/www/test/index.test.ts
+++ test/e2e/typescript-workspaces-paths/packages/www/test/index.test.ts
@@ -2,11 +2,52 @@
 
 import { join } from 'path'
 import { nextTestSetup } from 'e2e-utils'
+import { readFileSync, writeFileSync } from 'fs'
+import * as os from 'os'
 
 describe('TypeScript Features', () => {
-  describe('default behavior', () => {
+  describe.each([
+    [
+      { label: '', testBaseUrl: true },
+      { label: ' without baseUrl', testBaseUrl: false },
+    ],
+  ])('default behavior$label', ({ testBaseUrl }) => {
+    const tsConfigPath = join(__dirname, '../../../tsconfig.json')
+    let previousTsConfigJson: string | null = null
+    beforeAll(() => {
+      if (testBaseUrl) {
+        // Store the original tsconfig.json content
+        previousTsConfigJson = readFileSync(tsConfigPath, 'utf-8')
+        const tsconfig = JSON.parse(previousTsConfigJson)
+
+        delete tsconfig.baseUrl
+        tsconfig.compilerOptions.paths = {
+          'isomorphic-unfetch': ['./packages/www/types/unfetch.d.ts'],
+          '@c/*': ['./packages/www/components/*'],
+          '@lib/*': ['./packages/lib/a/*', './packages/lib/b/*'],
+          '@mycomponent': ['./packages/www/components/hello.tsx'],
+          'd-ts-alias': [
+            './packages/www/components/alias-to-d-ts.d.ts',
+            './packages/www/components/alias-to-d-ts.tsx',
+          ],
+        }
+        writeFileSync(tsConfigPath, JSON.stringify(tsconfig, null, 2) + os.EOL)
+      }
+    })
+
+    afterAll(() => {
+      if (previousTsConfigJson !== null) {
+        writeFileSync(tsConfigPath, previousTsConfigJson)
+      }
+    })
+
     const { next } = nextTestSetup({
       skipDeployment: true,
+      dependencies: testBaseUrl
+        ? {
+            typescript: '5.9.3',
+          }
+        : undefined,
       files: join(__dirname, '../../../'),
       subDir: 'packages/www',
     })
