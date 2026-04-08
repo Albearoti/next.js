77ee514bad49cfa2429395fd570d1ced72207ea3
Tim Neutkens <tim@timneutkens.nl>
2026-04-08 12:11:25 +0200

    Node.js streams: Add forkpoint for renderWithRestartOnCacheMissInValidation

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -6380,6 +6380,9 @@ async function renderWithRestartOnCacheMissInValidation(
 }> {
   const { componentMod: ComponentMod } = ctx
   const { clientModules } = getClientReferenceManifest()
+  const renderFlightStream = process.env.__NEXT_USE_NODE_STREAMS
+    ? renderToNodeFlightStream
+    : renderToWebFlightStream
 
   let startTime = -Infinity
   let requestStore: RequestStore = initialRequestStore
@@ -6444,7 +6447,7 @@ async function renderWithRestartOnCacheMissInValidation(
 
       const stream = workUnitAsyncStorage.run(
         requestStore,
-        renderToWebFlightStream,
+        renderFlightStream,
         ComponentMod,
         initialRscPayload,
         clientModules,
@@ -6545,7 +6548,7 @@ async function renderWithRestartOnCacheMissInValidation(
 
       const stream = workUnitAsyncStorage.run(
         requestStore,
-        renderToWebFlightStream,
+        renderFlightStream,
         ComponentMod,
         finalRscPayload,
         clientModules,
